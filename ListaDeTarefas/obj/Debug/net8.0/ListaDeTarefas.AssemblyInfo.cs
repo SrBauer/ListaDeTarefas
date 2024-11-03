d54182e429e24c4708f0b681c1ddcbd312c69ac9
SrBauer <bauer.jonathan.jj@gmail.com>
2024-11-03 17:39:20 -0300

    Alteração no filtro

--- ListaDeTarefas/obj/Debug/net8.0/ListaDeTarefas.AssemblyInfo.cs
+++ ListaDeTarefas/obj/Debug/net8.0/ListaDeTarefas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ListaDeTarefas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd3ae3a5954783876c1e748ffb182e869efdbbc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35663781583ef102b37417d5211891c81ab2235d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ListaDeTarefas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ListaDeTarefas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
